d9c2249883ef398b0ff0526d4b01e79305bf2a5e
Omulosi <mulongojohnpaul@gmail.com>
2020-03-03 18:13:11 +0300

    update settings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -174,9 +174,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
     
 
 import django_heroku
