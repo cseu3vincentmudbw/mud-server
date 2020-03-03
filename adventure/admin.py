6d7d2e648046cfd15b86a06ac4a51fcdec54f375
Omulosi <mulongojohnpaul@gmail.com>
2020-03-03 21:56:15 +0300

    update room model

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from .models import Room, Player
+
+admin.site.site_header = 'Legends MUD API'
+
 
 admin.site.register(Player)
 admin.site.register(Room)
+admin.site.unregister(Group)
